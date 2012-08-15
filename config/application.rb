da2cc447d34e8d686d27b8586bbcacb7f47ba617
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2012-08-15 10:37:41 -0700

    more heroku setup

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module HedgeyeEngineering
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Adding for Heroku
+    config.assets.initialize_on_precompile = true
   end
 end
